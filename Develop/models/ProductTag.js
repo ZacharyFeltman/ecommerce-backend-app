8c21085b7b8fe1f1d1e3cf19ea4f0c7b5b5d4f25
ZacharyFeltman <zachary.feltman@gmail.com>
2022-09-22 17:38:57 -0600

    did models

---
+++ Develop/models/ProductTag.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
