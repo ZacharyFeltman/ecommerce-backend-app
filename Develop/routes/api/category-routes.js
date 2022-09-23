aaf06368c6d788ddd3f3ba3fba8c48ba26f08c78
ZacharyFeltman <zachary.feltman@gmail.com>
2022-09-22 18:33:24 -0600

    working on routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,11 +6,17 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({include: Product}).then(categories => {
+    res.json(categories)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then(category => {
+    res.json(category)
+  })
 });
 
 router.post('/', (req, res) => {
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
+require('dotenv').config();
 // import sequelize connection
+const sequelize = require('./config/connection');
+// import{sequelize} from 'config/connection';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+async function sync() {
+  await sequelize.sync({force: false})
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+}
+
+sync()
